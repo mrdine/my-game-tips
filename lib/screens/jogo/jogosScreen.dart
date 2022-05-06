2fba2467a7ccefcd8f7435bc9d71017d4fec358b
Douglas Lima <douglas.felipelima18@gmail.com>
2022-05-06 14:01:05 -0300

    Adicionando scafold na tela meus jogos

--- lib/screens/jogo/jogosScreen.dart
+++ lib/screens/jogo/jogosScreen.dart
@@ -9,16 +9,19 @@ class JogosScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: GridView.count(
-            // Create a grid with 2 columns. If you change the scrollDirection to
-            // horizontal, this produces 2 rows.
-            crossAxisCount: 2,
-            children: myGames
-                .map((jogo) => Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: JogoItem(jogo: jogo),
-                    ))
-                .toList()));
+    return Scaffold(
+      appBar: AppBar(title: Text("Meus jogos")),
+      body: Center(
+          child: GridView.count(
+              // Create a grid with 2 columns. If you change the scrollDirection to
+              // horizontal, this produces 2 rows.
+              crossAxisCount: 2,
+              children: myGames
+                  .map((jogo) => Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: JogoItem(jogo: jogo),
+                      ))
+                  .toList())),
+    );
   }
 }
